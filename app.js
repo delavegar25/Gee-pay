78714d8fd1aa3b1626f73d0ce0e0c38c2ed3a845
Pascal <pascalokereke18@gmail.com>
2024-01-30 00:32:20 +0100

    vibration=notification

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 // javascript
 // moon light effect
 
-document.getElementById('moon-moon').addEventListener('click', () => {
+
+document.querySelector('#moon-moon').addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
+
+    // set a timer to revert  to light theme after a certain time.
+    setTimeout(() => {
+        document.body.classList.remove('dark-theme');
+    }, 5000);
 });
+
+
+// notification 
+document.getElementById('notification').addEventListener('click', ()=> {
+    let bell = this;
+    bell.classList.add('vibrate');
+
+// remove the class after the animation ends
+bell.addEventListener('animationend', () => {
+    bell.classList.remove('vibrate');
+})
+
+})
